4cb24ae03d85e1325a98a2cd23b01a196e9bb964
Abbass <abbassmortazavi@gmail.com>
2023-06-09 11:32:49 +0330

    add pinia and userStore

---
+++ src/store/UserStore.js
@@ -0,0 +1,48 @@
+import {
+    defineStore
+} from 'pinia'
+import axios from "axios";
+export const useUserStore = defineStore('user', {
+    state: () => ({
+        id: null,
+        first_name: null,
+        last_name: null,
+        email: null,
+        location: null,
+        image: null,
+        description: null,
+        token: null
+    }),
+
+    actions: {
+        async setUserDetails(res) {
+            this.$state.id = res.data.user.id,
+            this.$state.first_name = res.data.user.first_name,
+            this.$state.last_name = res.data.user.last_name,
+            this.$state.token = res.data.token,
+            this.$state.email = res.data.user.email,
+            this.$state.location = res.data.user.location,
+            this.$state.description = res.data.user.description
+        },
+
+        async fetchUser() {
+            let res = await axios.get(`http://localhost:8082/api/user/show/${this.$state.id}`);
+            this.$state.id = res.data.user.id,
+            this.$state.first_name = res.data.user.first_name,
+            this.$state.last_name = res.data.user.last_name,
+            this.$state.email = res.data.user.email,
+            this.$state.location = res.data.user.location,
+            this.$state.description = res.data.user.description
+        },
+        clearUser() {
+            this.$state.id = null
+            this.$state.first_name = null,
+            this.$state.last_name = null,
+            this.$state.token = null,
+            this.$state.email = null,
+            this.$state.location = null,
+            this.$state.description = null
+        }
+    },
+    persist: true
+});
